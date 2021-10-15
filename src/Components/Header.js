f639ae2bffbc5cd1df4ff5ae7e4162dc2c915233
Ryan <snorekr@gmail.com>
2021-10-15 10:43:54 -0400

    Working on Pizza Form component, added form elements: size, sauce

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
     return (
         <header>
-            <h1>Lambda Eats</h1>
+            <h1 className='logo'>Lambda Eats</h1>
             <nav>
-                <Link exact to='/'>Home</Link>
-                <Link to='/help'>Help</Link>
+                <NavLink exact to='/'>Home</NavLink>
+                <NavLink to='/help'>Help</NavLink>
             </nav>
         </header>
     )
--- src/Components/PizzaForm.js
+++ src/Components/PizzaForm.js
@@ -7,6 +7,32 @@ export default function PizzaForm() {
         <Header/>
         <body>
             <h3>Build Your Own Pizza</h3>
+            <img src='../Assets/Pizza.jpg' alt='Pizza Header'/>
+            <form>
+                <h4>Build You Own Pizza</h4>
+                <div className='subheader'>
+                    <h4>Choice of Size</h4>
+                    <p>Required</p>
+                </div>
+                <select name='size'>
+                    <option value=''>Select</option>
+                    <option value='small'>Small</option>
+                    <option value='medium'>Medium</option>
+                    <option value='large'>Large</option>
+                    <option value='massive'>MASSIVE</option>
+                </select>
+                <div className='subheader'>
+                    <h4>Choice of Sauce</h4>
+                    <p>Required</p>
+                </div>
+                <div className='sauce'>
+                    <label><input name='red' type='radio'></input>Original Red</label>
+                    <label><input name='ranch' type='radio'></input>Garlic Ranch</label>
+                    <label><input name='bbq' type='radio'></input>BBQ Sauce</label>
+                    <label><input name='alfredo' type='radio'></input>Spinach Alfredo</label>
+                </div>
+                
+            </form>
         </body>
     </>
     )
